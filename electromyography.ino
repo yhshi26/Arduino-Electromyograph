29e5b6c376afb68899a020e5ff4fb1b684ca8e47
Yosra Hashim <36962808+yhshi26@users.noreply.github.com>
2022-10-27 19:25:15 +0100

    Update electromyography.ino

--- electromyography.ino
+++ electromyography.ino
@@ -1,11 +1,13 @@
-int EMGPin = A1;
+int EMGPin = A0;
 int EMGVal = 0;
 
 void setup() {
   Serial.begin(115200);
+  Serial.println(EMGVal);
 }
 
 void loop() {
   EMGVal = analogRead(EMGPin);
   Serial.println(EMGVal);
+  delay(1);a
 }
